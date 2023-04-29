b1dbc1fd2370f5a62d7a322a5f5bc2f32ddc7c98
nntvi <nntvi.2609@gmail.com>
2023-04-29 23:53:45 +0700

    update genericType for input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,17 +1,18 @@
 import { InputHTMLAttributes, useState } from 'react'
-import { RegisterOptions, UseFormRegister } from 'react-hook-form'
+import { FieldPath, FieldValues, RegisterOptions, UseFormRegister } from 'react-hook-form'
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps<TFieldValues extends FieldValues> extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
-  register?: UseFormRegister<any>
+  register?: UseFormRegister<TFieldValues>
   rules?: RegisterOptions
   autoComplete?: string
   classNameInput?: string
   classNameError?: string
   classNameEye?: string
+  name: FieldPath<TFieldValues>
 }
 
-export default function Input({
+export default function Input<TFieldValues extends FieldValues>({
   errorMessage,
   className,
   name,
@@ -21,7 +22,7 @@ export default function Input({
   classNameError = 'mt-1 min-h-[1.25rem] text-sm text-red-600',
   classNameEye = 'absolute top-[13px] right-[10px] h-3 w-3 cursor-pointer',
   ...rest
-}: InputProps) {
+}: InputProps<TFieldValues>) {
   const registerResult = register && name ? register(name, rules) : null
   const [visibleEye, setVisibleEye] = useState(false)
 
