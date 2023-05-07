4e3fb61a99504c407d4e718355e828fe4c5a7b7a
nntvi <nntvi.2609@gmail.com>
2023-05-07 13:26:08 +0700

    test validate form login

--- src/pages/Login/Login.test.tsx
+++ src/pages/Login/Login.test.tsx
@@ -1,21 +1,46 @@
 import matchers from '@testing-library/jest-dom/matchers'
-import { screen, waitFor } from '@testing-library/react'
+import { fireEvent, screen, waitFor } from '@testing-library/react'
 import path from 'src/constant/path'
 import { logScreen, renderWithRouter } from 'src/utils/testUI'
-import { describe, expect, it } from 'vitest'
+import { beforeAll, describe, expect, it } from 'vitest'
 
 // để xài được toBeInTheDocument thì import matcher
 expect.extend(matchers)
 describe('Login', () => {
-  it('Hiển thị lỗi required khi không nhập gì', async () => {
-    const { user } = renderWithRouter({ route: path.login })
+  beforeAll(async () => {
+    renderWithRouter({ route: path.login })
     await waitFor(() => {
       expect(screen.queryByPlaceholderText('Email')).toBeInTheDocument()
     })
+  })
+  it('Hiển thị lỗi required khi không nhập gì', async () => {
     const submitButton = document.querySelector('form button[type="submit"]') as Element
-    user.click(submitButton)
-    expect(await screen.findByText('Email là bắt buộc')).toBeTruthy()
-    expect(await screen.findByText('Mật khẩu là bắt buộc')).toBeTruthy()
+    fireEvent.click(submitButton)
+    await waitFor(async () => {
+      expect(await screen.findByText('Email là bắt buộc')).toBeTruthy()
+      expect(await screen.findByText('Mật khẩu là bắt buộc')).toBeTruthy()
+    })
+    // await logScreen()
+  })
+
+  it('Hiển thị lỗi không đúng định dạng', async () => {
+    const emailInput = document.querySelector('form input[type="email"]') as HTMLInputElement
+    const passwordInput = document.querySelector('form input[type="password"]') as HTMLInputElement
+    const btnSubmit = document.querySelector('form button[type="submit"]') as HTMLButtonElement
+
+    fireEvent.change(emailInput, {
+      target: {
+        value: 'test'
+      }
+    })
+    fireEvent.change(passwordInput, {
+      target: {
+        value: '123'
+      }
+    })
+    fireEvent.click(btnSubmit)
+    expect(await screen.findByText('Email không đúng định dạng')).toBeTruthy()
+    expect(await screen.findByText('Độ dài từ 5-160 ký tự')).toBeTruthy()
     // await logScreen()
   })
 })
